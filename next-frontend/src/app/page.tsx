83409e8f9b52f6f5d51341289948e5dc7c3e26d1
Shivam Verma <mcrafter147@gmail.com>
2025-06-23 23:48:19 +0530

    landing page

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -2,9 +2,97 @@ import React from 'react'
 
 function page() {
   return (
-    <main>
-      <h1>Welcome to Artistly</h1>
-      <p>A platform to connect Event Planners with Performing Artists.</p>
+    <main className='w-full h-screen relative'>
+      <img
+        src="./hero-bg.png"
+        alt="hero background"
+        className='absolute object-cover w-full h-full inset-0 top-0 left-0 right-0 -z-10'
+      />
+      <img
+        src="./hero-free-hand.png"
+        alt="hero free hand"
+        className='absolute top-[40%] right-[48%] scale-[60%]'
+      />
+      <img
+        src="./hero-arrow.png"
+        alt="hero arrow"
+        className='absolute bottom-[5%] right-[40%] scale-[60%]'
+      />
+      <div className='z-10 relative flex items-center justify-between h-full px-24'>
+        <div className="text-[#fff] px-6 pt-18 min-h-screen flex flex-col justify-center w-[50%] gap-6">
+          {/* Heading */}
+          <h1 className="text-6xl font-bold leading-tight">
+            Brilliant Events,<br />
+            Magical <span className="text-[#b7f37b]">Experiences</span>
+          </h1>
+
+          {/* Subheading */}
+          <p className="text-lg text-[#dee4e3] pr-8">
+            Discover talented artists for your next event. From singers to speakers, we make it easy to find, connect, and book the perfect performer all in one place.
+          </p>
+
+          {/* CTA + Contact */}
+          <div className="flex flex-wrap items-center gap-8">
+            <button className="bg-[#b7f37b] text-[#174f46] text-md font-semibold px-6 py-3 rounded-2xl hover:brightness-125 transition cursor-pointer">
+              Explore Artists
+            </button>
+
+            <div className="text-sm">
+              <p className="text-[#dee4e3]">Any time call</p>
+              <p className="text-[#dee4e3] font-semibold text-lg">📞 +1-123-456-7290</p>
+            </div>
+          </div>
+
+          {/* Badges */}
+          <div className="flex gap-12 mt-20 flex-wrap">
+            <div>
+              <p className="font-bold">⭐⭐ Trustpilot</p>
+              <p className="text-sm text-[#dee4e3]">Always 5 Star</p>
+            </div>
+            <div>
+              <p className="font-bold">⭐ Capterra</p>
+              <p className="text-sm text-[#dee4e3]">Always 4.3 Star</p>
+            </div>
+          </div>
+        </div>
+        <div className='h-full w-[50%] flex items-center justify-center pt-18 relative'>
+          <img
+            src="./hero-img.png"
+            alt="hero free hand"
+            className='w-[80%]'
+          />
+          <div className="flex items-center gap-3 bg-white rounded-2xl px-4 py-4 border border-[#dbe2ea] shadow absolute bottom-[20%] left-0 z-[100]">
+            <div className="flex -space-x-6">
+              <img
+                src="/user1.avif"
+                alt="User 1"
+                className="w-12 h-12 rounded-full border-2 border-white object-cover"
+              />
+              <img
+                src="/user2.avif"
+                alt="User 2"
+                className="w-12 h-12 rounded-full border-2 border-white object-cover"
+              />
+              <img
+                src="/user3.avif"
+                alt="User 3"
+                className="w-12 h-12 rounded-full border-2 border-white object-cover"
+              />
+              <img
+                src="/user4.avif"
+                alt="User 4"
+                className="w-12 h-12 rounded-full border-2 border-white object-cover"
+              />
+            </div>
+
+            {/* Text */}
+            <div className="text-[#174f46]">
+              <p className="font-bold leading-none text-xl mb-1">50K+</p>
+              <p className="text-md leading-none">Satisfied Customers</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <a href="/artists">Explore Artists</a>
 
       <h2>Categories</h2>
---
+++ next-frontend/src/components/nav.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav className="flex justify-between items-center p-2 px-16 bg-[#174f46] z-100 top-0 left-0 right-0 fixed">
+            <a href='/' className="font-bold text-[28px] mr-8 text-[#b7f37b]">Artistly</a>
+            <ul className="flex gap-6 list-none m-0 p-0 font-semibold">
+                <li>
+                    <a href="/" className="no-underline text-[#dee4e3]">Home</a>
+                </li>
+                <li>
+                    <a href="/gallery" className="no-underline text-[#dee4e3]">Artists</a>
+                </li>
+                <li>
+                    <a href="/about" className="no-underline text-[#dee4e3]">Onboarding</a>
+                </li>
+                <li>
+                    <a href="/about" className="no-underline text-[#dee4e3]">About Us</a>
+                </li>
+            </ul>
+            <div className="flex items-center">
+                <button className="bg-[#174f46] border border-[#b7f37b] text-[#b7f37b] font-semibold py-3 px-6 rounded-2xl hover:bg-[#b7f37b] hover:text-[#174f46] transition-colors duration-300 cursor-pointer flex items-center">
+                    Let's talk
+                    <svg xmlns="http://www.w3.org/2000/svg" className="inline w-6 h-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03 8-9 8a9.77 9.77 0 01-4-.8L3 21l1.8-4A7.963 7.963 0 013 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
+                </button>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
