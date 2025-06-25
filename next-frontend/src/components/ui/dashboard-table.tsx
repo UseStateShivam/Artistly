c862654c8dbd12b1112c9aea342f5b2da47b1b62
Shivam Verma <mcrafter147@gmail.com>
2025-06-25 15:15:43 +0530

    Update dashboard-table.tsx

--- next-frontend/src/components/ui/dashboard-table.tsx
+++ next-frontend/src/components/ui/dashboard-table.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type Column<T> = {
   header: string;
   accessor: keyof T;
-  render?: (value: any, row: T) => React.ReactNode;
+  render?: (value: T[keyof T], row: T) => React.ReactNode;
   className?: string;
 };
 
